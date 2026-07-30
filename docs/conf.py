d5764f98080bccada1e6c39b2802f30cf307708e
Emily E Meyer <emeyer121@gmail.com>
2026-07-30 17:39:00 -0400

    Revert "made template for pooling windows object"
    This reverts commit 11122852ab27928409fc0bf386fcd4cac489f31d.

--- docs/conf.py
+++ docs/conf.py
@@ -3,10 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Import packages ---------------------------------------------------------
-
-import inspect
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -75,59 +71,5 @@ html_static_path = ["_static"]
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 
-# SPHINX CROSS REFERENCES
-add_function_parentheses = False
-
 # Enable automatic stub page generation
 autosummary_generate = True
-
-
-# this sphinx event allows us to have fine-grained control over whether to document
-# objects or not
-# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#event-autodoc-skip-member
-# however, we need an extra step to determine which of *our* objects the
-# object-to-document is attached to (https://github.com/sphinx-doc/sphinx/issues/9533)
-def skip_torch_inherited_methods(app, obj_type, name, obj, skip, options):
-    if obj_type in ("method", "property", "attribute"):
-        docobj = None
-        for frame in inspect.stack():
-            if frame.function == "_get_members":
-                docobj = frame.frame.f_locals["obj"]
-        if docobj is None:
-            raise Exception(
-                "Stack of sphinx events has changed, so unsure how to"
-                " grab object that corresponds to this method! See "
-                "PR #413 for discussion."
-            )
-        docobj_module = getattr(docobj, "__module__", "")
-        # we skip the attributes inherited from torch.nn.Module for our models and model
-        # components (found in the process module, we probably never want to show these
-        # attributes, but this is a more conservative way of doing this)
-        if docobj_module is not None and (docobj_module.startswith("fenestration")):
-            if obj_type == "method":
-                obj_module = getattr(obj, "__module__", "")
-                if obj_module is not None and obj_module.startswith("torch.nn.modules"):
-                    return True
-            else:
-                # for some reason, training doesn't show up as inherited (in the
-                # following set up or as part of the autodoc's inherited_members that we
-                # have access to in the jinja templates), so we exclude it manually
-                if name == "training":
-                    return True
-                # unlike methods, can't just check the module of an attribute, since it
-                # will typically be a basic type (e.g., bool). instead, we go through
-                # all the classes of docobj and see which one contains the attribute
-                # (https://stackoverflow.com/a/42503785/4659293)
-                obj_module = None
-                for cls in docobj.mro():
-                    if name in vars(cls):
-                        obj_module = cls.__module__
-                if obj_module is not None and obj_module.startswith("torch.nn.modules"):
-                    return True
-    return None
-
-
-# connect our custom method to the sphinx events callback API:
-# https://www.sphinx-doc.org/en/master/extdev/event_callbacks.html
-def setup(app):
-    app.connect("autodoc-skip-member", skip_torch_inherited_methods)
